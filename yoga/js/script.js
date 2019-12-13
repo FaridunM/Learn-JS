6bcc7a366c50082611579b0bcb35ed9e2323ddf9
Faridun <FaridunMarufi_96@mail.ru>
2019-12-13 16:35:02 +0500

    create dynamic timer

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -36,5 +36,50 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // timer
 
+    let deadline = '2020-01-01';
+
+    function difDates(endtime) {
+        let dif = Date.parse(deadline) - Date.parse(new Date()),
+            seconds = Math.floor(dif/1000) % 60,
+            minutes = Math.floor(dif/1000/60) % 60,
+            hours = Math.floor(dif/1000/60/60) % 60,
+            days = Math.floor(dif/(1000*60*60*24));
+
+        return {
+            'total' : dif,
+            'day' : days,
+            'hour' : hours,
+            'second' : seconds,
+            'minute' : minutes
+        };
+    }
+
+    function dinamicLayout(id, endtime) {
+        let timer = document.getElementById(id),
+        days = timer.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateTimer, 1000);
+
+        function updateTimer(endtime) {
+            let newTime = difDates(endtime);            
+            seconds.textContent = newTime.second <= 9 ? '0' + newTime.second : newTime.second;
+            minutes.textContent = newTime.minute <= 9 ? '0' + newTime.minute : newTime.minute;
+            hours.textContent = newTime.hour <= 9 ? '0' + newTime.hour : newTime.hour;
+            days.textContent = newTime.day <= 9 ? '0' + newTime.day : newTime.day;
+
+            if (newTime.total <= 0) {
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    dinamicLayout('timer', deadline);
 });
