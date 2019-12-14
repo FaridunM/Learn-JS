19ce8f9adfd70d07e43741ea7d3c77c2391d9a50
Faridun <FaridunMarufi_96@mail.ru>
2019-12-14 19:02:00 +0500

    new element was created like object class Option. Bringing the code to the standard ES6

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    header.addEventListener('mouseover', function(e) {
+    header.addEventListener('mouseover', (e) => {
         let target = e.target,
             tabs = header.children;
             
@@ -62,10 +62,8 @@ window.addEventListener('DOMContentLoaded', function() {
         hours = timer.querySelector('.hours'),
         minutes = timer.querySelector('.minutes'),
         seconds = timer.querySelector('.seconds'),
-        timeInterval = setInterval(updateTimer, 1000);
-
-        function updateTimer(endtime) {
-            let newTime = difDates(endtime);            
+        timeInterval = setInterval((endtime) => {
+            let newTime = difDates(endtime);
             seconds.textContent = newTime.second <= 9 ? '0' + newTime.second : newTime.second;
             minutes.textContent = newTime.minute <= 9 ? '0' + newTime.minute : newTime.minute;
             hours.textContent = newTime.hour <= 9 ? '0' + newTime.hour : newTime.hour;
@@ -78,8 +76,58 @@ window.addEventListener('DOMContentLoaded', function() {
                 minutes.textContent = '00';
                 seconds.textContent = '00';
             }
-        }
+        }, 1000);
+
+        // function updateTimer(endtime) {
+        //     let newTime = difDates(endtime);            
+        //     seconds.textContent = newTime.second <= 9 ? '0' + newTime.second : newTime.second;
+        //     minutes.textContent = newTime.minute <= 9 ? '0' + newTime.minute : newTime.minute;
+        //     hours.textContent = newTime.hour <= 9 ? '0' + newTime.hour : newTime.hour;
+        //     days.textContent = newTime.day <= 9 ? '0' + newTime.day : newTime.day;
+
+        //     if (newTime.total <= 0) {
+        //         clearInterval(timeInterval);
+        //         days.textContent = '00';
+        //         hours.textContent = '00';
+        //         minutes.textContent = '00';
+        //         seconds.textContent = '00';
+        //     }
+        // }
     }
 
     dinamicLayout('timer', deadline);
+
+    // modal
+
+    const more = document.querySelector('.more'),
+        moreTab = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+        
+    more.addEventListener('click', () => {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    moreTab.forEach((item) => {
+        item.addEventListener('click', () => {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    close.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        moreTab.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
 });
