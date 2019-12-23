6097edc4c0702b0e733e685de2020a927280fd93
Faridun <FaridunMarufi_96@mail.ru>
2019-12-23 10:33:18 +0500

    get the data from contact form by promises

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -77,57 +77,116 @@ window.addEventListener('DOMContentLoaded', function() {
                 seconds.textContent = '00';
             }
         }, 1000);
-
-        // function updateTimer(endtime) {
-        //     let newTime = difDates(endtime);            
-        //     seconds.textContent = newTime.second <= 9 ? '0' + newTime.second : newTime.second;
-        //     minutes.textContent = newTime.minute <= 9 ? '0' + newTime.minute : newTime.minute;
-        //     hours.textContent = newTime.hour <= 9 ? '0' + newTime.hour : newTime.hour;
-        //     days.textContent = newTime.day <= 9 ? '0' + newTime.day : newTime.day;
-
-        //     if (newTime.total <= 0) {
-        //         clearInterval(timeInterval);
-        //         days.textContent = '00';
-        //         hours.textContent = '00';
-        //         minutes.textContent = '00';
-        //         seconds.textContent = '00';
-        //     }
-        // }
     }
 
     dinamicLayout('timer', deadline);
 
     // modal
 
-    const more = document.querySelector('.more'),
+    let more = document.querySelector('.more'),
         moreTab = document.querySelectorAll('.description-btn'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
         
-    more.addEventListener('click', () => {
+    more.addEventListener('click', function() {
         overlay.style.display = 'block';
-        this.classList.add('more-splash');
+        this.classList.add('more-splash');          // Если стрелочная ф-ия это 1-ая ф-ия то у неё не будет своего this.
         document.body.style.overflow = 'hidden';
     });
 
-    close.addEventListener('click', () => {
-        overlay.style.display = 'none';
-        more.classList.remove('more-splash');
-        document.body.style.overflow = '';
-    });
-
-    moreTab.forEach((item) => {
-        item.addEventListener('click', () => {
+    moreTab.forEach((item) => {                     // Если стрелочная ф-ия это 1-ая ф-ия то у неё не будет своего this.
+        item.addEventListener('click', function() {
             overlay.style.display = 'block';
             this.classList.add('more-splash');
             document.body.style.overflow = 'hidden';
         });
     });
 
-    close.addEventListener('click', () => {
+    close.addEventListener('click', function() {
         overlay.style.display = 'none';
-        moreTab.classList.remove('more-splash');
+        this.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
 
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро мы с вами свяжемся',
+        fail: 'Что-то пошло не так!'
+    };
+
+    let form = document.querySelector('.main-form'),
+        contactForm = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    function sendData(element) {
+        element.addEventListener('submit', event => {
+            event.preventDefault();
+            element.appendChild(statusMessage);
+    
+            let formData = new FormData(element),
+                obj = {};
+    
+            formData.forEach((value, key) => {
+                obj[key] = value;
+            });
+    
+            let jsonData = JSON.stringify(obj);
+    
+            function passData(data) {
+                return new Promise(function(resolve, reject) {
+                    // --- For work with requests you need the local server (like open server / mamp) ---
+                    let requst = new XMLHttpRequest();
+                    requst.open('POST', 'server.php');
+                    requst.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                    // requst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    
+                    requst.onreadystatechange = function() {
+                        if (requst.readyState === 4) {
+                            requst.status == 200 ? resolve() : reject();
+                        }
+                    }
+    
+                    requst.send(data);
+                })
+            }
+    
+            function removeInputValue() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+    
+            passData(jsonData)            
+            .then(() => statusMessage.textContent = message.success)
+            .catch(() => statusMessage.textContent = message.fail)
+            .then(removeInputValue());
+        });
+    }
+
+    sendData(form);
+    sendData(contactForm);
 });
+
+
+
+// ---------- How to recive and post in JSON-format -----------
+
+// let options = {
+//     width: 1000,
+//     height: 500,
+//     background: 'red',
+//     font: {
+//         size: '14px',
+//         color: '#fff'
+//     }
+// };
+
+// let codeToJSON = JSON.stringify(options);
+// console.log(JSON.stringify(options), 'превращение в JSON-формат для передачи');
+
+// console.log(JSON.parse(codeToJSON);, 'раскодировка из JSON-формата');
